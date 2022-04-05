7347942bc3d5697aa64ab0d17b750e0d0e914ba7
Antonio Alfonso <antonioalfonso.work@gmail.com>
2022-04-05 17:06:08 +0200

    fix: mlp abi

--- src/abis/mlp.abi.ts
+++ src/abis/mlp.abi.ts
@@ -1,6 +1,6 @@
 export const MiniLiquidityProviderABI = [
   'function getLpTokenAddress() view external returns (address)',
-  'function lock(uint256 amountOutMin, uint256 deadline) external payable',
-  'function unlock(uint256 lockIndex, uint256 lockID, uint256 lockAmount, uint256 amountAMin, uint256 amountBMin, uint256 amountOutMin, uint256 deadline) external',
-  'function getTotalValueLocked() view external returns (uint256  totalWBNB, uint256 totalHUDI)',
+  'function getLPTokensOut(uint256 amount) public view returns (uint256 lpTokensAmount)',
+  'function addLiquidity(uint256 amountOutMin, uint256 deadline) external payable returns (uint256 amountA, uint256 amountB, uint256 liquidity)',
+  'function removeLiquidity(uint256 amountToRemove, uint256 amountTokenMin, uint256 amountETHMin, uint256 deadline)',
 ];

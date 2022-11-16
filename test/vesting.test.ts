6df72b4f71b3e945de89d95a9e9c8622ed84b36f
leobartowski <francescodangelo95@gmail.com>
2022-11-16 16:32:29 +0100

    done: improvements service

--- test/vesting.test.ts
+++ test/vesting.test.ts
@@ -45,7 +45,7 @@ async function main() {
     console.log('vesting', vesting);
 
     // GET THE CLAIMABLE AMOUNT
-    const amount = await vestingService.getClaimableAmount(
+    const amount = await vestingService.claimVesting(
       CONTRACT_OWNER_PRIVATE_KEY,
       vestingIds[0],
     );
